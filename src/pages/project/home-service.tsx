fb778e947e2c9480df00dcf2ff1ee72d74c08a2a
PotterWinter <natthanataa@gmail.com>
2025-02-03 13:10:39 +0700

    fix: update project data for apply job 101

--- src/pages/project/home-service.tsx
+++ src/pages/project/home-service.tsx
@@ -3,7 +3,7 @@ import { projectsData } from "@/components/project-data";
 import { ProjectSection101 } from "@/components/project-section101";
 import { ProjectSection102 } from "@/components/project-section102";
 import { ProjectSectionImage101 } from "@/components/project-section-image101";
-import { ProjectSectionCode101 } from "@/components/project-section-code101";
+// import { ProjectSectionCode101 } from "@/components/project-section-code101";
 // import { ProjectSectionCode102 } from "@/components/project-section-code101";
 
 export default function HomeServiceProject({}) {
@@ -39,33 +39,28 @@ export default function HomeServiceProject({}) {
               <section className="flex flex-col mt-20 gap-12 max-w-full">
                 <ProjectSectionImage101
                   image={project.image}
-                  content="form this project has main feature about user to booking and select the service form website and start form the homepage use can't see the other service if don't login user can't see any service without first 3 service and its about authentication"
+                  content="Project Overview Main Features:
+Booking various services such as cleaning, air conditioning repair, plumbing repair, etc. &&
+Searching for services by name, price, and service type && Booking management && Administration system management && Member system and authentication && Payment system && Service status system"
                 />
-                <ProjectSectionCode101
+                {/* <ProjectSectionCode101
                   filepath={"Example code"}
-                  coding={`              <ProjectSection101
-                title={project.title}
-                content={project.content}
-                techStack={project.techStack}
-                responsibility={project.responsibility}
-                description={project.description}
-                year={project.year}
-              />
-              <ProjectSection102
-                linkGitHub={project.linkGitHub}
-                linkUrl={project.linkUrl}
-                linkBahance={project.linkBahance}
-              />`}
-                  content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit aspernatur nihil, in voluptates repellendus, earum laboriosam laborum consequuntur nobis facilis rerum dolorem iste reiciendis adipisci distinctio. Odit iste voluptatem, laborum numquam autem totam perspiciatis porro "
-                />
-                {/* <ProjectSectionCode102
-                filepath01="abcabc"
-                coding01={`NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY=pk_test_51QV6WHIvN7RAky1NJlvbXvmZWx8VOhycCqPFBsM9jARIgvdJmYLi33fl95AWVq3oaoG6HxtOCHlv7L0GF96Ls7Sm00E79R5TpF`}
-                filepath02=""
-                coding02={`
-NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY=pk_test_51QV6WHIvN7RAky1NJlvbXvmZWx8VOhycCqPFBsM9jARIgvdJmYLi33fl95AWVq3oaoG6HxtOCHlv7L0GF96Ls7Sm00E79R5TpF
+                  coding={`<ProjectSection101
+  title={project.title}
+  content={project.content}
+  techStack={project.techStack}
+  responsibility={project.responsibility}
+  description={project.description}
+  year={project.year}
+/>
+<ProjectSection102
+  linkGitHub={project.linkGitHub}
+  linkUrl={project.linkUrl}
+  linkBahance={project.linkBahance}
+/>
 `}
-                content=""/> */}
+                  content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit aspernatur nihil, in voluptates repellendus, earum laboriosam laborum consequuntur nobis facilis rerum dolorem iste reiciendis adipisci distinctio. Odit iste voluptatem, laborum numquam autem totam perspiciatis porro "
+                /> */}
               </section>
             </div>
           </main>
@@ -74,3 +69,13 @@ NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY=pk_test_51QV6WHIvN7RAky1NJlvbXvmZWx8VOhycCqPF
     </>
   );
 }
+
+
+{/* <AdminSubmitPopUp
+setShowPopUpSubmit={setShowPopUpSubmit}
+showPopUpSubmit={showPopUpSubmit}
+message="สร้างรายการสำเร็จ"
+subMessage="กรุณากดยืนยันเพื่อกลับสู่หน้าหลัก ?"
+confirmationText="ยืนยัน"
+redirectPath="/adminservice"
+/> */}
